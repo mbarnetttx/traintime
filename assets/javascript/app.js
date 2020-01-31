4ea2000ed6a9f017e282268cac0001da73f46423
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-01-30 22:12:18 -0700

    added click function, added alias names for firebase, added capture of inputs, added function to collect info from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,21 +6,53 @@ var firebaseConfig = {
     storageBucket: "traincatch-d4aa5.appspot.com",
     messagingSenderId: "439222683318",
     appId: "1:439222683318:web:f5fd1b5ee49ada47e58817"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.databse();
+var database = firebase.databse();
 
-  var trainName = "";
-  var destination = "";
-  var frequency = 0;
-  var nextArrival = 0;
-  var minAway = 0;
-  var input = "";
-  var addTrain = {
-      name: trainName,
-      dest: destination,
-      freq: frequency,
-      time: nextArrival
-  }
+var trainName = "";
+var destination = "";
+var frequency = 0;
+var nextArrival = 0;
+var minAway = 0;
+var input = "";
+var addTrain = {
+    name: trainName,
+    dest: destination,
+    freq: frequency,
+    time: nextArrival
+}
+
+$(".submit").on("click", function (ev) {
+//  event.preventdefault()
+
+    var newTrain = {
+        name: trainName,
+        dest: destination,
+        first: nextArrival,
+        freq: frequency,
+    }
+
+    firebaseconfig.push(newTrain);
+    
+    var trainName = $("#trainname").val().trim();
+    var destination $("#destination").val().trim();
+    var nextArrival = $("#nextarrival").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().dest;
+    var nextArrival = childSnapshot.val().first;
+    var frequency = childSnapshot.val().freq;
+
+
+
+
+    var  = moment(firstTime, "HH:mm")
+
+   
+
+
+})
